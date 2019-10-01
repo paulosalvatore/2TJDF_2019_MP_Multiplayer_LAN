02b16935d2bd6ecaff2f6ab414fa324e12d24783
Paulo Salvatore <paulo@salvatore.tech>
2019-10-01 12:14:49 -0300

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour
+{
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        if (isLocalPlayer)
+        {
+            var h = Input.GetAxis("Horizontal");
+
+            transform.Translate(Vector3.left * (h * Time.deltaTime));
+        }
+    }
+}
