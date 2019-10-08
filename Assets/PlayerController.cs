e83a0979e138539f57e50e5e1ea99e0346da84b4
Paulo Salvatore <paulo@salvatore.tech>
2019-10-08 12:05:36 -0300

    Players colors & Spawning cubes

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,8 +5,37 @@ using UnityEngine.Networking;
 
 public class PlayerController : NetworkBehaviour
 {
+    public Material materialVermelho;
+    public Material materialAzul;
+
+    public GameObject cuboPrefab;
+
+    public float velocidade = 3f;
+    public float velocidadeRotacao = 200f;
+
     private void Start()
     {
+        Material material = null;
+
+        if (isServer && isLocalPlayer)
+        {
+            material = materialVermelho;
+        }
+        else if (isServer && !isLocalPlayer)
+        {
+            material = materialAzul;
+        }
+        else if (!isServer && isLocalPlayer)
+        {
+            material = materialAzul;
+        }
+        else if (!isServer && !isLocalPlayer)
+        {
+            material = materialVermelho;
+        }
+
+        var meshRenderer = GetComponent<MeshRenderer>();
+        meshRenderer.material = material;
     }
 
     private void Update()
@@ -14,8 +43,31 @@ public class PlayerController : NetworkBehaviour
         if (isLocalPlayer)
         {
             var h = Input.GetAxis("Horizontal");
+            var v = Input.GetAxis("Vertical");
 
-            transform.Translate(Vector3.left * (h * Time.deltaTime));
+            transform.Translate(
+                Vector3.forward * (velocidade * v * Time.deltaTime)
+            );
+
+            transform.Rotate(
+                Vector3.up * (velocidadeRotacao * h * Time.deltaTime)
+            );
+
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                CmdSpawnarCubo();
+            }
         }
+    }
+
+    [Command]
+    private void CmdSpawnarCubo()
+    {
+        var cubo = Instantiate(cuboPrefab);
+
+        cubo.transform.position = transform.position + (transform.forward * 2);
+        cubo.transform.rotation = transform.rotation;
+
+        NetworkServer.Spawn(cubo);
     }
 }
